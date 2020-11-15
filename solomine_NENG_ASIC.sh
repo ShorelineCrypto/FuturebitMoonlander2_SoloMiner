b50a65cec17ba9af2946b9eedeb8d5c1b49d43e1
ShorelineCrypto <admin@shorelinecrypto.com>
2020-11-15 11:03:14 -0500

    add experimental feature to boost rewards on reset

--- solomine_NENG_ASIC.sh
+++ solomine_NENG_ASIC.sh
@@ -6,4 +6,12 @@
 
 # rotate on two full nodes
 # base diff <  1
-/home/user/FutureBit/bfgminer_5.4.2-futurebit2_linux_x86_64/bfgminer --scrypt --url=127.0.0.1:8376  -u  wallet2user   -p wallet2passwd  --url=127.0.0.1:8378 -u wallet5user -p  wallet5passwd  --retries  -1  --rotate 1 --no-stratum  --net-delay -S /dev/ttyUSB0 --set MLD:clock=384
+/home/user/FutureBit/bfgminer_5.4.2-futurebit2_linux_x86_64/bfgminer --scrypt --url=127.0.0.1:8376  -u  wallet2user   -p wallet2passwd  --url=127.0.0.1:8378 -u wallet5user -p  wallet5passwd  --retries  -1  --no-submit-stale  --rotate 1 --no-stratum  --net-delay -S /dev/ttyUSB0 --set MLD:clock=384
+
+## Experimental, more rewards on resets, but still have some "ALL POOL DEAD" error triggered
+# /home/user/FutureBit/bfgminer_5.4.2-futurebit2_linux_x86_64/bfgminer --scrypt --url=127.0.0.1:8376  -u  wallet2user   -p wallet2passwd  --url=127.0.0.1:8378 -u wallet5user -p  wallet5passwd  --retries  -1  --expiry 10 --no-submit-stale   --rotate 1 --no-stratum  --net-delay -S /dev/ttyUSB0 --set MLD:clock=384
+
+# base diff > 1
+# /home/user/FutureBit/bfgminer_5.4.2-futurebit2_linux_x86_64/bfgminer --scrypt --url=127.0.0.1:8376  -u  wallet2user   -p wallet2passwd  --url=127.0.0.1:8378 -u wallet5user -p  wallet5passwd  --retries  -1  --no-submit-stale  --rotate 1 --no-stratum  --net-delay -S /dev/ttyUSB0 --set MLD:clock=600
+
+
