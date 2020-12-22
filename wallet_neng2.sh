ab05a6ff8392abf0bd8011d70db089abf8f790ac
ShorelineCrypto <admin@shorelinecrypto.com>
2020-12-22 18:51:18 -0500

    updated to nengcoin version/addnode

--- wallet_neng2.sh
+++ wallet_neng2.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # QT
-#/home/user/nengcoin_v1.3.0.1_ubuntu16.04/nengcoin-qt -port=8377  -rpcport=8376 -datadir=/home/user/.nengcoin2
+#/home/user/nengcoin_v1.5.0.0_ubuntu16.04/nengcoin-qt -port=8377  -rpcport=8376 -datadir=/home/user/.nengcoin2
 
 ## base difficulty reset:  0.000244 - 1 diff, GPU/USB ASIC solo mining period
 ##  > 1 diff - period for big ASIC
-/home/user/nengcoin_v1.3.0.1_ubuntu16.04/nengcoind -port=8377  -rpcport=8376 -datadir=/home/user/.nengcoin2
+/home/user/nengcoin_v1.5.0.0_ubuntu16.04/nengcoind -port=8377  -rpcport=8376 -datadir=/home/user/.nengcoin2
 
--- wallet_neng5.sh
+++ wallet_neng5.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-~/nengcoin_v1.3.0.1_ubuntu16.04/nengcoind -port=8379  -rpcport=8378 -datadir=/home/user/.nengcoin5
+~/nengcoin_v1.5.0.0_ubuntu16.04/nengcoind -port=8379  -rpcport=8378 -datadir=/home/user/.nengcoin5
 
